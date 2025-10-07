f47793199be3d1ec5be7601d2dbead915bc1bf36
Ayushman Gupta <AyushmanGupta@bdo.in>
2025-10-07 23:54:42 +0530

    requirement modified

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,16 @@
 import streamlit as st
-def checkCode (user_input:str,language:str) :
+import requests
+def checkCode (language:str,code:str) :
+    try :
+        payload = {"language":language,"code":user_input}
+        api_url = 
+        reponse = requests.post(api_url,json=payload)
+        reponse.raise_for_status()
+        data = reponse.json()
+        return data
+    except requests.exceptions.RequestException as e :
+        st.error(f"Error calling API: {e}")
+        return data
 
 st.title("Check for the Syntax,Compilation and Logical Error")
 user_input = st.text_area("Enter your code here:")
